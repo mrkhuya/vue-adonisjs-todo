782418526c98ca2309f0bf0ed478172df413f91e
Thanh Huynh <thanhhuynh@azwebplus.com>
2019-02-15 14:22:18 +0700

    Task / CRUD

--- server/start/routes.js
+++ server/start/routes.js
@@ -17,7 +17,7 @@
 const Route = use('Route')
 
 Route.get('/', () => {
-  return { greeting: 'Hi, Vue.Js & Adonis.Js Todolist' }
+  return { greeting: 'Vue.Js & Adonis.Js Todolist app' }
 })
 
 Route.group(() => {
@@ -27,5 +27,11 @@ Route.group(() => {
   Route.get('projects', 'ProjectController.index').middleware('auth')
   Route.post('projects', 'ProjectController.create').middleware('auth')
   Route.delete('projects/:id','ProjectController.destroy').middleware('auth')
+  Route.patch('projects/:id','ProjectController.update').middleware('auth')
+
+  Route.get('projects/:id/tasks','TaskController.index').middleware('auth')
+  Route.post('projects/:id/tasks','TaskController.create').middleware('auth')
+  Route.patch('tasks/:id','TaskController.update').middleware('auth')
+  Route.delete('tasks/:id','TaskController.destroy').middleware('auth')
 })
   .prefix('api/v0')
